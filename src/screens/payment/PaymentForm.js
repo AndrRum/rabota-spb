63be664c89592cb00260952d88a3ac7e0815a927
olgaPaskar <kisya157@mail.ru>
2023-05-22 19:55:11 +0300

    fix payment form

--- src/screens/payment/PaymentForm.js
+++ src/screens/payment/PaymentForm.js
@@ -1,10 +1,17 @@
-import React, {useRef} from 'react';
+import React, {useEffect} from 'react';
 import "./PaymentForm.css";
-import {usePayMutation} from "../../redux/paymentApi";
 
 export const PaymentForm = () => {
-    const formRef = useRef(null);
-    const [pay, {isLoading}] = usePayMutation();
+
+    useEffect(() => {
+        const script = document.createElement('script');
+        script.src = 'https://securepay.tinkoff.ru/html/payForm/js/tinkoff_v2.js';
+        script.async = true;
+        document.body.appendChild(script);
+        return () => {
+            document.body.removeChild(script);
+        };
+    }, []);
 
     const tinkoffPay = (form) => {
         let name = form.description.value;
@@ -14,7 +21,7 @@ export const PaymentForm = () => {
         let quantity = 1;
 
         if (amount) {
-            form.receipt.value = JSON.stringify({
+          form.receipt.value = JSON.stringify({
                 Email: email,
                 Phone: phone,
                 EmailCompany: 'test@tinkoff.ru',
@@ -38,95 +45,30 @@ export const PaymentForm = () => {
     };
 
     const payHelper = (form) => {
-        // pay(form)
-        //     .then((result) => {
-        //         console.log(result);
-        //     })
-        //     .catch((error) => {
-        //         console.error(error);
-        //     });
+
     };
 
     return (
-        <div className="tinkoffPayWidget">
-            <form
-                ref={formRef}
-                name="TinkoffPayForm"
-                className="tinkoffPayForm"
-                onSubmit={(e) => {
-                    tinkoffPay(formRef.current);
-                    e.preventDefault();
-                }}
-            >
-                <input
-                    className="tinkoffPayRow"
-                    type="hidden"
-                    name="terminalkey"
-                    value="1684231603937"
-                />
-                <input
-                    className="tinkoffPayRow"
-                    type="hidden"
-                    name="frame"
-                    value="false"
-                />
-                <input
-                    className="tinkoffPayRow"
-                    type="hidden"
-                    name="language"
-                    value="ru"
-                />
-                <input
-                    className="tinkoffPayRow"
-                    type="hidden"
-                    placeholder="Номер заказа"
-                    name="order"
-                />
-                <input
-                    className="tinkoffPayRow"
-                    type="text"
-                    placeholder="Сумма заказа"
-                    id="tinkoffAmount"
-                    name="amount"
-                    required
-                />
-                <input
-                    className="tinkoffPayRow"
-                    type="text"
-                    placeholder="Описание заказа"
-                    name="description"
-                    required
-                />
-                <input
-                    className="tinkoffPayRow"
-                    type="hidden"
-                    placeholder="ФИО"
-                    name="name"
-                />
-                <input
-                    className="tinkoffPayRow"
-                    type="text"
-                    placeholder="E-mail"
-                    id="tinkoffEmail"
-                    name="email"
-                    required
-                />
-                <input
-                    className="tinkoffPayRow"
-                    type="text"
-                    placeholder="Телефон"
-                    id="tinkoffPhone"
-                    name="phone"
-                />
-                <input
-                    className="tinkoffPayRow"
-                    type="hidden"
-                    id="tinkoffReceipt"
-                    name="receipt"
-                    value=""
-                />
-                <input className="tinkoffPayRow payBtn" type="submit" value="Оплатить"/>
+        <div class="tinkoffPayWidget">
+            <form name="TinkoffPayForm" class="tinkoffPayForm" onSubmit={(e) => {
+                tinkoffPay(e.target)
+                e.preventDefault()
+            }}>
+                <input class="tinkoffPayRow" type="hidden" name="terminalkey" value="1684231603937"/>
+                <input class="tinkoffPayRow" type="hidden" name="frame" value="false"/>
+                <input class="tinkoffPayRow" type="hidden" name="language" value="ru"/>
+                <input class="tinkoffPayRow" type="hidden" placeholder="Номер заказа" name="order"/>
+                <input class="tinkoffPayRow" type="text" placeholder="Сумма заказа" id="tinkoffAmount" name="amount"
+                       value="" required/>
+                <input class="tinkoffPayRow" type="text" placeholder="Описание заказа" name="description" required/>
+                <input class="tinkoffPayRow" type="hidden" placeholder="ФИО" name="name"/>
+                <input class="tinkoffPayRow" type="text" placeholder="E-mail" id="tinkoffEmail" name="email"
+                       required/>
+                <input class="tinkoffPayRow" type="text" placeholder="Телефон" id="tinkoffPhone"
+                       name="phone"/>
+                <input class="tinkoffPayRow" type="hidden" id="tinkoffReceipt" name="receipt" value=''/>
+                <input class="tinkoffPayRow payBtn" type="submit" value="Оплатить"/>
             </form>
         </div>
-    );
-};
+    )
+}
